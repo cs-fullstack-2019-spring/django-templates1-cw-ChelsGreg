0c4f09a38d1039668a512c5183133f1e45618c33
ChelsGreg <cgreg2217@gmail.com>
2019-02-22 16:18:24 -0600

    Complete

--- tempProj/tempApp/urls.py
+++ tempProj/tempApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .import views
 
+# DIRECTING ROUTES
 urlpatterns = [
     path('home/base/', views.toBase, name='base'),
     path('home/about/', views.toAbout, name='about'),
--- tempProj/tempApp/views.py
+++ tempProj/tempApp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# FUNCTIONS FOR ROUTES
+
+# INDEX
 def index(request):
     return render(request, 'tempApp/index.html')
 
-
+# BASE
 def toBase(request):
     return render(request, 'tempApp/base.html')
 
-
+# ABOUT
 def toAbout(request):
     return render(request, 'tempApp/about.html')
